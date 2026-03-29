aa4c1810f1ea3ad3895b2c3e2715c7acda57af59
viniciuslnunes <viniciuslopes_nunes@hotmail.com>
2026-03-29 17:10:39 -0300

    wip

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,7 +10,7 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
--- index.js
+++ index.js
@@ -31,4 +31,4 @@ for (const file of eventFiles) {
   }
 }
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
